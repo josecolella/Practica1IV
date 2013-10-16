a96b8a7836ccd4c10da711105bba17288bc4ac30
josecolella <josecolella@yahoo.com>
2013-10-16 12:07:45 +0200

    Changed it so that user that has registered don't have to sign in

--- php/php/insertUser.php
+++ php/php/insertUser.php
@@ -4,13 +4,14 @@
 	// ini_set('display_errors', true);
 	// error_reporting(E_ALL);
 	/**
-	 * Este script sirve para insertar un usuario dentro 
+	 * Este script sirve para insertar un usuario dentro
 	 * de una base de datos
-	 * 
+	 *
 	 * @author Jose Miguel Colella <josecolella@correo.ugr.es>
 	 * @version 1.0
 	 */
-	
+	session_start()
+
 	include_once('UserVerification.php');
 
 	$user = new UserVerification();
@@ -18,7 +19,10 @@
 	$user->setInsertVariables();
 	$user->insertUser();
 
+  $_SESSION['username'] = $user->getUserName();
+
 	$user->close();
+
 
 	echo "<script>alert('Registración Completada'); location.href='../index.php';</script>";
 	//Redireccion a la pagina principal. El usuario tiene una sesion establecida
